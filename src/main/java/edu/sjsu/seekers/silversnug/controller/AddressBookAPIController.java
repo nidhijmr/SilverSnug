c937b1229db17a5bfa415cf0eaaf8c0e97847c77
Ashwinisnv <ashwini.snv@gmail.com>
2019-02-17 13:38:03 -0800

    Renaming Address book controller and service

--- src/main/java/edu/sjsu/seekers/silversnug/controller/AddressBookAPIController.java
+++ src/main/java/edu/sjsu/seekers/silversnug/controller/AddressBookAPIController.java
@@ -3,28 +3,28 @@ package edu.sjsu.seekers.silversnug.controller;
 import edu.sjsu.seekers.silversnug.request.AddressBookRequest;
 import edu.sjsu.seekers.silversnug.response.AddressBookResponse;
 import edu.sjsu.seekers.silversnug.response.GenericResponse;
-import edu.sjsu.seekers.silversnug.service.SilverSnugService;
+import edu.sjsu.seekers.silversnug.service.AddressBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class SilverSnugAPIController {
+public class AddressBookAPIController {
 
     @Autowired
-    SilverSnugService silverSnugService;
+    AddressBookService addressBookService;
 
     @PostMapping("/SilverSnug/Address/addAddress")
     @ResponseBody
     public GenericResponse addAddress(@RequestBody AddressBookRequest request) {
-        return silverSnugService.saveAddress(request);
+        return addressBookService.saveAddress(request);
 
-        //        silverSnugService.authenticate("Ashwini");
+        //        addressBookService.authenticate("Ashwini");
         //        return "Hello from Silver Snug!";
     }
 
     @GetMapping("/SilverSnug/Address/getAddress")
     public AddressBookResponse getAddress(String userName) {
-        return silverSnugService.getAddressByUserName(userName);
+        return addressBookService.getAddressByUserName(userName);
     }
 
 }
--- src/main/java/edu/sjsu/seekers/silversnug/service/AddressBookService.java
+++ src/main/java/edu/sjsu/seekers/silversnug/service/AddressBookService.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SilverSnugService {
+public class AddressBookService {
 
     private final String SUCCESS = "SUCCESS";
     @Autowired
