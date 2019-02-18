a4b0eeea9954e183002b350f68e5937fa053ee75
Anuradha Rajashekar <31361652+AnuradhaIyer@users.noreply.github.com>
2019-02-18 13:59:00 -0800

    Update Constants.java

--- src/main/java/edu/sjsu/seekers/silversnug/util/Constants.java
+++ src/main/java/edu/sjsu/seekers/silversnug/util/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String SUCCESS_GET = "success";
     public static final String UNSUCCESSFUL_GET_USER = "No user found.";
     public static final String PILL_ADD_SUCCESS = "pill added sucessfully";
+    public static final String UNSUCCESSFUL_GET_PILL = "No pill added";
 }
