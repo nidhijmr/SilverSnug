de031d8a0e483fea656cb5a8070010a7791f573e
nidhijmr <nidhi.jmr@gmail.com>
2019-03-14 20:43:03 -0700

    updated message

--- src/main/java/edu/sjsu/seekers/silversnug/util/Constants.java
+++ src/main/java/edu/sjsu/seekers/silversnug/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String USERNAME_ALREADY_EXISTS = "user name already exists";
     public static final String USERNAME_DOES_NOT_EXISTS = "user name does not exists";
     public static final String SUCCESS_GET = "success";
-    public static final String UNSUCCESSFUL_GET_USER = "No user found.";
+    public static final String UNSUCCESSFUL_GET_USER = "Invalid username/credentials.";
     public static final String PILL_ADD_SUCCESS = "pill added sucessfully";
     public static final String UNSUCCESSFUL_GET_PILL = "No pill added";
     public static final String UNSUCCESSFUL_PILL_USER = "pills not found for this User";
