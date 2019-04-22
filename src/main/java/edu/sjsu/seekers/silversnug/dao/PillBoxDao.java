d6d0c0f527462db84c34d291c0ead33c538266cb
AnuradhaIyer <tulsiraj14@gmail.com>
2019-04-22 16:03:25 -0700

    Changing edit functionality

--- src/main/java/edu/sjsu/seekers/silversnug/dao/PillBoxDao.java
+++ src/main/java/edu/sjsu/seekers/silversnug/dao/PillBoxDao.java
@@ -64,7 +64,7 @@ public class PillBoxDao {
 
     public void editPill(EditPillRequest request)
     {
-        AmazonDynamoDB dynamoDB = dynamodbClient.getDynamoDB();
+        /*AmazonDynamoDB dynamoDB = dynamodbClient.getDynamoDB();
         DynamoDBMapper mapper = new DynamoDBMapper(dynamoDB);
 
         Map<String, AttributeValue> values = new HashMap<>();
@@ -83,8 +83,33 @@ public class PillBoxDao {
             if (request.getDosage()!=null)
                 pillBox.get(0).setDosage(request.getDosage());
             mapper.save(pillBox.get(0));
+        }*/
+
+        AmazonDynamoDB dynamoDB = dynamodbClient.getDynamoDB();
+        DynamoDBMapper mapper = new DynamoDBMapper(dynamoDB);
+
+        Map<String, AttributeValue> values = new HashMap<>();
+        values.put(":userId", new AttributeValue().withS(request.getUserId()));
+        values.put(":medicineName", new AttributeValue().withS(request.getMedicineName()));
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("userId = :userId and medicineName = :medicineName").withExpressionAttributeValues(values);
+
+        List<PillBox> pillBox = mapper.scan(PillBox.class, scanExpression);
+        if (!CollectionUtils.isNullOrEmpty(pillBox)) {
+
+            if(request.getMedicineName()!= null)
+                pillBox.get(0).setMedicineName(request.getMedicineName());
+            if(request.getDosage()!= null)
+                pillBox.get(0).setDosage(request.getDosage());
+            if(request.getPotency()!= null)
+                pillBox.get(0).setPotency(request.getPotency());
+            if(request.getNotes()!= null)
+                pillBox.get(0).setNotes(request.getNotes());
+            mapper.save(pillBox.get(0));
         }
     }
+
+
 
     public String getPillIdByPillName(String userId,String medicineName) {
         AmazonDynamoDB dynamoDB = dynamodbClient.getDynamoDB();
